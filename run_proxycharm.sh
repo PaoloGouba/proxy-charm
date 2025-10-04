a613c72bc33c8ce72d58320a5b1e4db224b20627
Paolo Gouba <pfgouba@gmail.com>
2025-10-04 12:47:50 +0200

    Daily update

--- run_proxycharm.sh
+++ run_proxycharm.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-# Esegui lo script Python
 python3 /Users/paologouba/Documents/Paolo/Goals/proxy-charm/src/main.py
-
-# Aggiorna la repository su GitHub
 cd /Users/paologouba/Documents/Paolo/Goals/proxy-charm
 git add .
 git commit -m "Daily update"
